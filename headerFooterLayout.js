207ce842561a7ba265738619983973647b08762f
ViktorZvonar <viktorzvonar@yahoo.com>
2023-08-15 14:32:53 +0200

    header-footer-almost-done

---
+++ headerFooterLayout.js
@@ -0,0 +1,15 @@
+export const headerFooterLayout = function () {
+  document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+      .then((response) => response.text())
+      .then((content) => {
+        document.getElementById("header-placeholder").innerHTML = content;
+      });
+
+    fetch("footer.html")
+      .then((response) => response.text())
+      .then((content) => {
+        document.getElementById("footer-placeholder").innerHTML = content;
+      });
+  });
+};
