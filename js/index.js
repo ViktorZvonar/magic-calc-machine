f89427eaddd81442a46d63e07285c9304f7b70ad
RAJ KUMAR THAPA <73226546+Raazk10@users.noreply.github.com>
2023-08-15 22:44:36 +0200

    Fix structure and path

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import { headerFooterLayout } from "./modules/headerFooterLayout.js";
+import Calculator from "./modules/calculator.js";
+
+headerFooterLayout();
+Calculator();
--- js/modules/headerFooterLayout.js
+++ js/modules/headerFooterLayout.js
@@ -1,12 +1,12 @@
 export const headerFooterLayout = function () {
   document.addEventListener("DOMContentLoaded", function () {
-    fetch("header.html")
+    fetch("../pages/header.html")
       .then((response) => response.text())
       .then((content) => {
         document.getElementById("header-placeholder").innerHTML = content;
       });
 
-    fetch("footer.html")
+    fetch("../pages/footer.html")
       .then((response) => response.text())
       .then((content) => {
         document.getElementById("footer-placeholder").innerHTML = content;
