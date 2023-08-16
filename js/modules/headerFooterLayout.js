a36eae4850499a119d97ba58b4c51c58e8340c3e
ViktorZvonar <viktorzvonar@yahoo.com>
2023-08-16 12:16:45 +0200

    fix

--- js/modules/headerFooterLayout.js
+++ js/modules/headerFooterLayout.js
@@ -1,12 +1,12 @@
 export const headerFooterLayout = function () {
   document.addEventListener("DOMContentLoaded", function () {
-    fetch("/header.html")
+    fetch("../header.html")
       .then((response) => response.text())
       .then((content) => {
         document.getElementById("header-placeholder").innerHTML = content;
       });
 
-    fetch("/footer.html")
+    fetch("../footer.html")
       .then((response) => response.text())
       .then((content) => {
         document.getElementById("footer-placeholder").innerHTML = content;
